c4244705d5d1c658c4de2e756bd86b99481a7a2e
venugopalakrishna-ch <venugopalakrishna.ch@gmail.com>
2020-05-11 17:04:09 +0530

    changes to test and main program

--- pythonic_garage_band/pythonic_garage_band.py
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -9,40 +9,42 @@ class Band:
         Band.count += 1          
 
     def play_solos(self):         
-        play_solos = ""                         
-        for i in range(len(self.members)):
-            musician = self.members[i]                          
-            play_solos += musician.name + " please play the solo: " + self.solos[i] + " "     
-            print(f"{musician.name} please play the solo: {self.solos[i]}")   
+        play_solos = ""                    
+        for obj in self.members:
+            play_solos += obj.name.strip("'") + " please play the solo: " + obj.play_solo() + " "
+            print(f"{obj.name} please play the solo: {obj.play_solo()}")
         return play_solos.strip()
             
     def __str__(self):
-        return "Band name is '{}'; and members of the Band Are: {}".format(self.name,self.members)
+        musicians = ""
+        for obj in self.members:
+            musicians += obj.name.strip("'") + ", "
+        return "Band name is '{}'; and members of the Band Are: {}".format(self.name,musicians.rstrip(", "))
 
     def __repr__(self):
-        return 'Band({},{})'.format(self.name,self.members)
+        return 'Band({},{},{})'.format(self.name,self.members,self.solos)
 
     def create_from_data(self,path):
         self.path = path
         class_args = []
+        musician_args = []
         input = open(self.path )         
         band_name = input.readline()        
         gitar = input.readline().split(":")
         bass = input.readline().split(":")
         drums = input.readline().split(":")
         solos = input.readline().split(":")  
-        class_args.append(band_name.strip())
-        class_args.append(gitar[0]+"('" + gitar[1].strip() + "')," + bass[0] + "('" + bass[1].strip() + "')," + drums[0] + "('"+ drums[1].strip() + "')")
-        class_args.append(solos)
-        #return "'{}',[{}('{}'),{}('{}'),{}('{}')],{}".format(band_name.strip(),gitar[0],gitar[1].strip(),bass[0],bass[1].strip(),drums[0],drums[1].strip(),solos)
+        musician_args.append(Guitarist("'"+gitar[1].strip()+"'"))
+        musician_args.append(Bassist("'"+bass[1].strip()+"'"))
+        musician_args.append(Drummer("'"+drums[1].strip()+"'"))
+        class_args.append(band_name.strip())        
+        class_args.append(musician_args)
+        class_args.append(solos)            
         return class_args
-
 
     @classmethod
     def to_list(cls):        
         return f"The number of Bands created: {cls.count}"     
-
-
 
 class Musician(ABC):
     def __init__(self,name,instrument):
@@ -100,7 +102,10 @@ if __name__ == "__main__":
      beatles = Band("The Beatles", [Guitarist("John Lennon"), Bassist("Paul McCartney"),Drummer("Siva Mani")],["Sympathy for the Devil","Light My Fire","Sharp Dressed Man"])        
      class_args = beatles.create_from_data("assets/band_input.txt")
      from_file  = Band(class_args[0],class_args[1],class_args[2])
-     print(beatles.play_solos())
+     print(from_file.__repr__())
+     print(from_file.__str__())
+     solos_test = beatles.play_solos()
+         
      
 
     
